b7e2aed353a1373a91882ca26deb433f7d8258b1
annasophie05 <seilbecka@gmail.com>
2024-11-22 16:54:06 +0100

    integrated APIKey

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     timeElement.innerHTML = formatDate(date);
     iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class="weather-app-icon"/>`;
+
+    getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -51,20 +53,33 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
-function displayForecast () {
-    
-    let days =["Tue", "Wed", "Thu", "Fri", "Sat"];
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days [date.getDay()];
+}
+
+function getForecast(city ){
+    let apiKey = "1a1c91004be79f6e4446ab6cad35o0tf";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`
+    axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast (response) {
     let forecastHtml = "";
 
-    days.forEach(function(day) {
+    response.data.daily.forEach(function(day, index) {
+        if (index < 5) {
         forecastHtml =
         forecastHtml +
         `<div class="weather-forecast-day"> 
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon"> ⛅</div>
-        <div class="weather-forecast-temperatures"> <div class="weather-forecast-temperature"><strong>15°</strong></div> <div class="weather-forecast-temperature"> 9° </div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+        <div class="weather-forecast-temperatures"> <div class="weather-forecast-temperature"><strong>${Math.round(day.temperature.maximum)}°</strong></div> <div class="weather-forecast-temperature"> ${Math.round(day.temperature.minimum)}° </div>
         </div>
      </div>`;
+        }
     });
 
     let forecastElement = document.querySelector("#forecast");
@@ -75,4 +90,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Munich");
-displayForecast();
